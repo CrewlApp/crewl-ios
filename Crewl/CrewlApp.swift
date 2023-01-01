0537bb8fc7e0f5aa6ad6eb75009cf2bbeba6a948
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-01 20:27:12 +0200

    feat: create project

---
+++ Crewl/CrewlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewlApp.swift
+//  Crewl
+//
+//  Created by NomoteteS on 1.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrewlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
